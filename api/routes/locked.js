122b17a2b36ac18e5325cd301f5b6258aa8221ef
Valtti Rinnemaa <valttirinnemaa@gmail.com>
2022-04-01 12:54:24 +0300

    Add card locked - functionality to REST API

---
+++ api/routes/locked.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const locked = require('../models/locked_model');
+
+router.get('/:cardnumber',
+function(request, response) {
+  locked.getByCardnumber(request.params.cardnumber, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult[0]);
+    }
+  });
+});
+
+router.put('/:cardnumber',
+function(request, response) {
+  locked.update(request.params.cardnumber, request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+module.exports = router;
