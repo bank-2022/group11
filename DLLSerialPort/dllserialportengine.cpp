b499b51978f44b072787224840d0405bff1681ba
EmilAhve <emil.ahvenainen@gmail.com>
2022-04-13 12:59:54 +0300

    [Add rfid dll code]

---
+++ DLLSerialPort/dllserialportengine.cpp
@@ -0,0 +1,41 @@
+#include "dllserialportengine.h"
+
+DLLSerialPortEngine::DLLSerialPortEngine(QObject *parent) : QObject(parent)
+{
+    objectQSerialPort = new QSerialPort(this);
+    objectQSerialPort->setPortName("COM8");
+    objectQSerialPort->setBaudRate(QSerialPort::Baud9600);
+    objectQSerialPort->setDataBits(QSerialPort::Data8);
+    objectQSerialPort->setParity(QSerialPort::NoParity);
+    objectQSerialPort->setStopBits(QSerialPort::OneStop);
+    objectQSerialPort->setFlowControl(QSerialPort::NoFlowControl);
+    //objectQSerialPort->open(QIODevice::ReadOnly);
+    QObject::connect(objectQSerialPort, SIGNAL(readyRead()), this, SLOT(serialReceived()));
+}
+
+DLLSerialPortEngine::~DLLSerialPortEngine()
+{
+    objectQSerialPort->close();
+    delete objectQSerialPort;
+    objectQSerialPort = NULL;
+}
+
+void DLLSerialPortEngine::serialReceived()
+{
+    // store serial data into a byte array
+    objectQByteArray = objectQSerialPort->readAll();
+
+    // convert into string
+    objectQString = QTextCodec::codecForMib(106)->toUnicode(objectQByteArray);
+
+    // trim excess
+    objectQString.remove(0, 3);
+    objectQString.remove(6, 4);
+    emit sendString(objectQString);
+}
+
+void DLLSerialPortEngine::openConnection()
+{
+    objectQSerialPort->open(QIODevice::ReadOnly);
+    objectQSerialPort->clear(QSerialPort::AllDirections);   // clear buffer before reading new data
+}
